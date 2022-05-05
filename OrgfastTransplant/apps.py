7497fe570c2954608501511472f2e63ee4d0aa34
Gautam Bharte <87375647+GautamBharte@users.noreply.github.com>
2022-05-05 22:08:56 +0530

    Delete OrgfastTransplant directory

--- OrgfastTransplant/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrgfasttransplantConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'OrgfastTransplant'
--- OrgfastTransplant/include.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-# include your path here
-hospital_data_path = Path('hospital_data.csv') 
-graph_info_path = Path('graph_info.csv')
